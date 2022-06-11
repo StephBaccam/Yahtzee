3391b374de47bd81f0eeb5a317137f37d9f308c5
Stéphane <stephane.baccam@orencash.fr>
2022-06-11 17:06:21 +0200

    2e test Red

--- Yahtzee/YahtzeeTests.cs
+++ Yahtzee/YahtzeeTests.cs
@@ -5,6 +5,18 @@ namespace Yahtzee
     public class YahtzeeTests
     {
         [Fact]
+        public void ReturnCountOnesIs3Points()
+        {
+            var yahtzee = new YahtzeeClass();
+            List<int> rolls = new List<int>()
+            {
+                1, 1, 2, 2, 3
+            };
+            var result = yahtzee.CountOnes(rolls);
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
         public void ReturnTrueIfFullHouse()
         {
             var yahtzee = new YahtzeeClass();
--- YahtzeeClasses/YahtzeeClass.cs
+++ YahtzeeClasses/YahtzeeClass.cs
@@ -2,6 +2,20 @@
 {
     public class YahtzeeClass
     {
+        public int CountOnes(List<int> rolls)
+        {
+            var resToReturn = 0;
+            foreach(var roll in rolls)
+            {
+                if(roll == 1)
+                {
+                    resToReturn += roll;
+                }
+            }
+
+            return resToReturn;
+        }
+
         public bool IsFullHouse(int roll1, int roll2, int roll3, int roll4, int roll5)
         {
             return true;
